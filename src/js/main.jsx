ffa278d1aa56a42e5e0cbc33b5ac8360157dbdb1
Oriol383 <oriolcm02@gmail.com>
2025-07-04 10:00:51 +0000

    firstcommit

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SecondsCounter from "/src/js/components/Home.jsx";
+import "/src/styles/index.css";
 
-//Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+function App() {
+  const [seconds, setSeconds] = React.useState(0);
 
-// index.css'
-import '../styles/index.css'
+  React.useEffect(() => {
+    // Contador empieza cuando la página termina de cargar
+    function handleLoad() {
+      setSeconds(0);
+      const interval = setInterval(() => {
+        setSeconds((sec) => sec + 1);
+      }, 1000);
+      // Limpiar el intervalo si el componente se desmonta (por buenas prácticas)
+      return () => clearInterval(interval);
+    }
 
-// components
-import Home from './components/Home';
+    if (document.readyState === "complete") {
+      handleLoad();
+    } else {
+      window.addEventListener("load", handleLoad);
+      return () => window.removeEventListener("load", handleLoad);
+    }
+  }, []);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+  return <SecondsCounter seconds={seconds} />;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
